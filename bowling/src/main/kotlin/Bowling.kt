2d62b880bd0194ed1ca94fa7f88e5d706c90a3fc
JT Wheeler <jt.wheeler@accenture.com>
2020-01-31 10:38:28 -0500

    Refactored

--- bowling/src/main/kotlin/Bowling.kt
+++ bowling/src/main/kotlin/Bowling.kt
@@ -3,16 +3,36 @@ class Bowling {
         var sum = 0
 
         for ((index, roll) in gameResults.withIndex()) {
-            sum += if (roll == 'X') {
-                10 + gameResults[index + 1].toString().toInt() + gameResults[index + 2].toString().toInt()
-            } else if (roll == '/') {
-                10 + gameResults[index + 1].toString().toInt()
-            } else {
-                roll.toString().toInt()
-            }
 
+            val currentRollValue = toNumericValue(roll)
+
+            sum += currentRollValue
+
+            sum += addBonus(roll, gameResults, index)
         }
 
         return sum
+    }
+
+    private fun addBonus(roll: Char, gameResults: String, index: Int): Int {
+        return when (roll) {
+            'X' -> {
+                toNumericValue(gameResults[index + 1]) + toNumericValue(gameResults[index + 2])
+            }
+            '/' -> {
+                toNumericValue(gameResults[index + 1])
+            }
+            else -> {
+                0
+            }
+        }
+    }
+
+    private fun toNumericValue(roll: Char): Int {
+        return when(roll) {
+            'X' -> 10
+            '/' -> 10
+            else -> roll.toString().toInt()
+        }
     }
 }
